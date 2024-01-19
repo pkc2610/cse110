3f2ee3edd93f6cfccee952378d3f40c7d3812e90
pkc2610 <phoebekayc@gmail.com>
2024-01-18 18:14:05 -0700

    did more stuff

--- birthday.eggs.cookies.cse110.py
+++ birthday.eggs.cookies.cse110.py
@@ -2,5 +2,19 @@ birthdayc=input("How old are you?")
 birthdayf=(int(birthdayc) + 1)
 
 print(" ")
-print("You are", birthdayc, "years old")
-print("On your next birthday, you will be", birthdayf, "years old!")
+print("On your next birthday, you will be", birthdayf, "!")
+
+
+print(" ")
+eggsa=input("How many egg cartons do you have?")
+eggsb=(int(eggsa)*12)
+
+print(" ")
+print("You have", eggsb, "eggs.")
+
+cookies=float(input("How many cookies do you have?"))
+people=float(input("How many people are there?"))
+cookperper=(float(cookies / people))
+
+print(" ")
+print("Each person can have", cookperper, "cookies.")
