1440747ba79f941f19c8699d490b547ad98974c1
pkc2610 <phoebekayc@gmail.com>
2024-01-18 23:06:08 -0700

    my hands are so worthless

---
+++ areas.cse110.py
@@ -0,0 +1,58 @@
+sq_side=float(input("What is the length of a side of the square in cm?"))
+
+sq_area=sq_side**2
+
+print(f"The area of the square is: {sq_area}")
+
+rec_length=float(input("What is the length of rectangle in cm?"))
+rec_width=float(input("What is the width of the rectangle in cm?"))
+rec_area=rec_length * rec_width
+
+print(f"The area of the rectangle is: {rec_area}")
+
+circ_radius=float(input("What is the radius of the circle in cm?"))
+circ_area=3.14 * (circ_radius ** 2)
+
+print(f"The area of the circle is: {circ_area}")
+
+circ_radius=float(input("What is the radius of the circle in cm?"))
+circ_area=3.14 * (circ_radius ** 2)
+
+print(f"The area of the circle is: {circ_area}")
+
+value=float(input("What is the value to be used?"))
+
+sq_area=value ** 2
+
+print(f"Area of a square: {sq_area}")
+
+circ_area=3.14 * (value ** 2)
+
+print(f"Area of a circle: {circ_area}")
+
+cube_vol=value ** 3
+
+print(f"Volume of a cube: {cube_vol}")
+
+sph_vol=(4 / 3) * 3.14 * (value ** 3)
+
+print(f"Volume of a sphere: {sph_vol}")
+
+sq_side=float(input("What is the length of a side of the square?"))
+sq_area=sq_side ** 2
+
+print(f"The area of the square is: {sq_area} cm^2")
+print(f"The area of the square is: {sq_area / 10000} m^2")
+
+rec_length=float(input("What is the length of rectangle?"))
+rec_width=float(input("What is the width of the rectangle?"))
+rec_area=rec_length * rec_width
+
+print(f"The area of the rectangle is: {rec_area} cm^2")
+print(f"The area of the rectangle is: {rec_area / 10000} m^2")
+
+radius=float(input("What is the radius of the circle (in cm)?"))
+circ_area=3.14 * (radius ** 2)
+
+print(f"The area of the circle is: {circ_area} cm^2")
+print(f"The area of the circle is: {circ_area / 10000} m^2")
