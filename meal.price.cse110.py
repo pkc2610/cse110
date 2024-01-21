214a432b015e5358b863261c41462ee00e6154a3
pkc2610 <phoebekayc@gmail.com>
2024-01-20 23:57:11 -0700

    finished up requirements

--- meal.price.cse110.py
+++ meal.price.cse110.py
@@ -6,3 +6,18 @@ adamt=(int(input("How many adults are there?")))
 subtotal=float((kidmeal*kidamt)+(admeal*adamt))
 
 print(f"Subtotal: ${subtotal:.2f} ")
+
+rate=float(input("What is the sales tax rate?"))
+
+total_sales_tax=rate*subtotal
+total=total_sales_tax + subtotal
+
+amount=float(input("What will be your payment amount?"))
+
+change=amount-total
+
+print(f"Your change is ${change:.2f}")
+
+print(" ")
+
+print("Thank you for coming!")
