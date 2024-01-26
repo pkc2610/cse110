03364c08acfdde7df04b58d61237601621353aab
pkc2610 <phoebekayc@gmail.com>
2024-01-25 22:49:13 -0700

    fingers crossed

--- grade.calculator.py
+++ grade.calculator.py
@@ -1,1 +1,7 @@
-grade = input("What is your grade percent? ")
+percent = input("What is your grade percent? ")
+if percent >= 90:
+    letter = "A"
+if percent >= 80:
+    letter = "B"
+if percent >= 70:
+    letter = "C"
