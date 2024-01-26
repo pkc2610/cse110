de36173588a6cec585fefe1709de2df6faf3f957
pkc2610 <phoebekayc@gmail.com>
2024-01-25 20:49:33 -0700

    python practice round 1

---
+++ greater.less.than.py
@@ -0,0 +1,14 @@
+number1=input("What is the first number?")
+number2=input("What is the second number?")
+
+if number2<number1:
+    print("The first number is greater.")
+else: print("The first number is not greater.")
+
+if number2==number1:
+    print("The numbers are equal.")
+else: print("the numbers are not equal.")
+
+if number1<number2:
+    print("The second number is greater.")
+else: print("The second number is not greater.")
