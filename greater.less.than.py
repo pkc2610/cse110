57d15b668031d833eb010b1193e8783e533009e2
pkc2610 <phoebekayc@gmail.com>
2024-01-25 20:57:18 -0700

    tried something

--- greater.less.than.py
+++ greater.less.than.py
@@ -12,3 +12,9 @@ else: print("the numbers are not equal.")
 if number1<number2:
     print("The second number is greater.")
 else: print("The second number is not greater.")
+
+animal=input("What is your favorite animal?")
+
+if animal=="dog":
+    print("That's my favorite, too!")
+else: print("That's not my favorite, but it's still a good one!!")
