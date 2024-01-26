30f64c361623f0e423962a1fb6f47fdda813ad82
pkc2610 <phoebekayc@gmail.com>
2024-01-25 23:34:21 -0700

    there we go 🥰

--- adventuregame.py
+++ adventuregame.py
@@ -4,5 +4,5 @@ choice1=str(input(print("Do you want to walk to the SIGN or SUGGEST a frog name?
 
 if choice1 == str("SIGN"):
     print("the sign says “Pottsfield, 1 Mile” Your stomach rumbles at the thought of a proper meal in some good old fashioned civilization. You’re about ready to start floating at even the thought of pumpkin pie, but your brother stops you—“I think there’s a bluebird over there!")
-elif: choice1 == str("SUGGEST")
+elif choice1 == str("SUGGEST"):
     print("your brother doesn’t consider your offhanded offers of “Gandalf” and “Benjamin Franklin Pierce” and insists you take a break and think of a good, proper name for this frog when all of a sudden—you see a figure moving through the forest out of the corner of your eye. It looks like they’re carrying a lantern, and you get the feeling you can ask him for directions.")
