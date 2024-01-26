7d18020e14c055f04f88cf87d186dbf396fc6f5b
pkc2610 <phoebekayc@gmail.com>
2024-01-25 22:54:07 -0700

    elif, like eilif, I do believe is my friend

--- grade.calculator.py
+++ grade.calculator.py
@@ -1,11 +1,11 @@
 percent = int(input("What is your grade percent? "))
 if percent >= 90:
     letter = "A"
-if percent >= 80:
+elif percent >= 80:
     letter = "B"
-if percent >= 70:
+elif percent >= 70:
     letter = "C"
-if percent >= 60:
+elif percent >= 60:
     letter = "D"
 else: letter = "F"
 
