77626daeb2a2eb8f56d73d7d77c5e468bb3a2668
pkc2610 <phoebekayc@gmail.com>
2024-01-25 23:29:46 -0700

    it pains me to save it like this but I gotta figure it out

---
+++ adventuregame.py
@@ -0,0 +1,8 @@
+print("You’re walking in the woods with your brother, who thinks the teapot on his head makes him look like an elephant.")
+print("He’s going over different options for his frog’s name when you think you see a sign.")
+choice1=str(input(print("Do you want to walk to the SIGN or SUGGEST a frog name?")))
+
+if choice1 == str("SIGN"):
+    print("the sign says “Pottsfield, 1 Mile” Your stomach rumbles at the thought of a proper meal in some good old fashioned civilization. You’re about ready to start floating at even the thought of pumpkin pie, but your brother stops you—“I think there’s a bluebird over there!”)
+eilif: choice1a=str(input(("Do you head for PIE or the BLUEBIRD?")))
+
