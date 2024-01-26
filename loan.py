d96eafbbf0f89295679bfe6bf7e225eff883f33f
pkc2610 <phoebekayc@gmail.com>
2024-01-25 22:03:42 -0700

    somehow I never realized python would be so much math (and I thought I was smart)

---
+++ loan.py
@@ -0,0 +1,10 @@
+loansize=input("How large is the loan?")
+credhis=input("How good is your credit history?")
+income=input("How high is your income?")
+downpay=input("How large is your down payment?")
+
+loandecision= False
+
+if loansize>=5:
+    if credhis>=7 and income>=7:
+        loandecision= True
