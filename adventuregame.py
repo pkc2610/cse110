ff022586b45199e2551af3ae148d87c43a7f925f
pkc2610 <phoebekayc@gmail.com>
2024-01-27 20:52:17 -0700

    here I go again by Whitesnake

--- adventuregame.py
+++
@@ -1,8 +0,0 @@
-print("You’re walking in the woods with your brother, who thinks the teapot on his head makes him look like an elephant.")
-print("He’s going over different options for his frog’s name when you think you see a sign.")
-choice1=str(input(print("Do you want to walk to the SIGN or SUGGEST a frog name?")))
-
-if choice1 == str("SIGN"):
-    print("the sign says “Pottsfield, 1 Mile” Your stomach rumbles at the thought of a proper meal in some good old fashioned civilization. You’re about ready to start floating at even the thought of pumpkin pie, but your brother stops you—“I think there’s a bluebird over there!")
-elif choice1 == str("SUGGEST"):
-    print("your brother doesn’t consider your offhanded offers of “Gandalf” and “Benjamin Franklin Pierce” and insists you take a break and think of a good, proper name for this frog when all of a sudden—you see a figure moving through the forest out of the corner of your eye. It looks like they’re carrying a lantern, and you get the feeling you can ask him for directions.")
