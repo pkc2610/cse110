c62824e86a912e3bf1a07cfef0673c6f5970e3f0
pkc2610 <phoebekayc@gmail.com>
2024-02-01 20:47:32 -0700

    I FIXED IT!! (it was simple but now my computer can rest a moment)

--- magic.number.py
+++ magic.number.py
@@ -5,11 +5,11 @@ magic_number = random.randint(1, 100)
 guess=-1
 
 while guess != magic_number:
-    print("What is your guess?")
+    guess = int(input("What is your guess? "))
 
-if guess < magic_number:
-     print("Higher!")
-elif guess > magic_number:
-     print("Lower.")
-else:
-    print("You got it!")
+    if guess < magic_number:
+        print("Higher!")
+    elif guess > magic_number:
+        print("Lower.")
+    else:
+        print("You got it!")
