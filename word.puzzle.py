65cb563b8b1b0d3b6a2114cf9fa66103532df567
pkc2610 <phoebekayc@gmail.com>
2024-02-01 22:48:16 -0700

    oh she's close but I'm clearly missing something

--- word.puzzle.py
+++ word.puzzle.py
@@ -11,7 +11,8 @@ print("Best of luck!!")
 guess=input("What is your guess?")
 guesses=1
 
-while guess != sec_word:
+while str(guess) != str(sec_word):
+    print("Your guess was not correct")
     guess=input("What is your guess?")
     guess=guesses+1
     
