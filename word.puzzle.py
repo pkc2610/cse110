972a18e49ed07842bfa576a96978f98614fc66f6
pkc2610 <phoebekayc@gmail.com>
2024-02-01 22:14:26 -0700

    giving instructions before I do any actual coding

---
+++ word.puzzle.py
@@ -0,0 +1,9 @@
+sec_word="flavortown"
+
+print("Welcome to the word guessing game!!")
+print()
+print("You'll need to write in 10-letter words until you guess the correct 10-letter word!")
+print()
+print("Just a heads up--this program does NOT account for duplicates. For example, if you \nenter the word 'food' and the answer is 'rose,' then the hint would show as '_Oo_,' \ndespite there only being one 'o' in 'rose.'")
+print()
+print("Best of luck!!")
