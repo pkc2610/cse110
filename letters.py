ea7340458f4f3ff1dbc73b8e7ef6038b3bed890a
pkc2610 <phoebekayc@gmail.com>
2024-02-01 20:13:07 -0700

    stuff

--- letters.py
+++ letters.py
@@ -2,3 +2,9 @@ word="commitment"
 
 favl=input(f"What is your favorite letter in {word}?")
 
+for letter in word:
+
+    if letter.lower() == favl:
+        print(letter.upper(), end="")
+    else:
+        print(letter.lower(), end="")
