8ca4233256dfc8c01b1ca1404a01d9e7d1139672
pkc2610 <phoebekayc@gmail.com>
2024-02-01 17:46:52 -0700

    there she is

--- first.loop.py
+++ first.loop.py
@@ -2,6 +2,6 @@ number=int(input("Please type a positive number "))
 
 while number < 0:
     print("Sorry, that is a negative number. Please try again.")
-    int(input("Please type a positive number."))
+    number = int(input("Please type a positive number: "))
 
-print("The number is {number}.")
+print(f"The number is: {number}")
