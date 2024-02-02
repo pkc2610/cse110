9735cf22c1d75245a08ce9c873472fc85b629e15
pkc2610 <phoebekayc@gmail.com>
2024-02-01 17:43:42 -0700

    after all that, the alligator was facing the wrong way

--- first.loop.py
+++ first.loop.py
@@ -1,5 +1,5 @@
 number=int(input("Please type a positive number "))
 
-while number > 0:
+while number < 0:
     print("Sorry, that is a negative number. Please try again.")
     int(input("Please type a positive number."))
