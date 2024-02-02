23647716be046f7ce96f16a8b9a0331778255565
pkc2610 <phoebekayc@gmail.com>
2024-02-01 17:40:03 -0700

    forgot to make it an int

--- first.loop.py
+++ first.loop.py
@@ -1,4 +1,4 @@
 number=input("Please enter a positive number ")
 
-while number >0:
-    print("Sorry, that is a negative number. Please try again.")
+while number > 0:
+    int(input("Sorry, that is a negative number. Please try again."))
