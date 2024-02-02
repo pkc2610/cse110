96e124576265c977bafe2e650d14894c605351a5
pkc2610 <phoebekayc@gmail.com>
2024-02-01 19:42:27 -0700

    un-redacting some things

--- colors.py
+++ colors.py
@@ -1,11 +1,11 @@
 colors = ["red", "blue", "green", "yellow"]
 list=range(4)
 
-#for list in colors:
-   # print(str(list))
+for list in colors:
+    print(str(list))
 
-#for lst in range(1, 9):
-    #print(lst)
+for lst in range(1, 9):
+    print(lst)
 
 for lt in range(2, 21, 2):
     print(lt)
