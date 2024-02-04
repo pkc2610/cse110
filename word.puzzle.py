6ac178e445f3ecf2ba61c3a1169ff01f150df8a6
pkc2610 <phoebekayc@gmail.com>
2024-02-03 22:09:26 -0700

    finally started running things before committing 😎

--- word.puzzle.py
+++ word.puzzle.py
@@ -1,24 +1,37 @@
 word="flavortown"
-guess = input("What is your guess?")
 guesses=1
 
 print("Welcome to the word guessing game!!")
-print()
-print("You'll need to write in 10-letter words until you guess the correct 10-letter word!")
 print()
 print("Just a heads up--this program does NOT account for duplicates. For example, if you \nenter the word 'food' and the answer is 'rose,' then the hint would show as '_Oo_,' \ndespite there only being one 'o' in 'rose.'")
 print()
 print("Best of luck!!")
 
-
+guess=input("What is your guess?")
 while len(guess) != len(word):
     print("Your guess was not correct")
-    guess=input("What is your guess?")
+    guess=input("Try again!")
     guesses=guesses+1
 
 
 guess = guess.lower()
 
-    
+while word != guess:
+
+    thing = ""
+
+    for i in range(len(word)):
+        if word[i] == guess[i]:
+            thing = thing + guess[i].upper()
+        elif word.__contains__(guess[i]):
+            thing = thing + guess[i].lower()
+        else:
+            thing = thing + '_'
+
+    print(f"Your guess is {thing}")
+    guess=input("Try again!")
+    guess = guess.lower()
+
+    guesses=guesses+1
 print("You got it!!")
 print(f"It took you {guesses} guesses")
