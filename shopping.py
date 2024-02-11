f285b279684fba500111a0d023f9beadb124181e
pkc2610 <phoebekayc@gmail.com>
2024-02-10 18:28:55 -0700

    prarie dogs are perfect

--- shopping.py
+++ shopping.py
@@ -12,3 +12,9 @@ while item != "end":
 print("\nThe shopping list is:")
 for item in shoppinglist:
     print(item)
+
+
+print("\nThe shopping list with indexes is:")
+for i in range(len(shoppinglist)):
+    item = shoppinglist[i]
+    print(f"{i}. {item}")
