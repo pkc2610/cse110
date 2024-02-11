5e5ff048b2bf8ecaf1c15e3db686dc35b4064016
pkc2610 <phoebekayc@gmail.com>
2024-02-10 18:26:10 -0700

    let's hope this is good

--- shopping.py
+++ shopping.py
@@ -3,3 +3,8 @@ print("Please enter the items of the shopping list (type: quit to finish):")
 shoppinglist = []
 item = None
 
+while item != "end":
+    item = input("Item: ")
+
+    if item != "end":
+        shoppinglist.append(item)
