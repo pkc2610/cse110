0bc0317bae10bf6f3c813ca2c05fd38c05473f32
pkc2610 <phoebekayc@gmail.com>
2024-02-10 20:48:37 -0700

    adding the instructions as a comment so I don't have to keep going back and forth

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,3 +1,17 @@
+#Store prices as well as names.
+#Change the add functionality to also ask for and store the price of the item.
+#Change the display functionality to also display the prices of the items.
+#When displaying the items, display a number in front of each item. The numbers should start with 1.
+#Complete the option to display the total amount of the prices of all the items in the shopping cart.
+#Whenever prices are displayed, they should be shown to two decimal places and include the appropriate currency symbol (for example $, €, etc.)
+#Complete the option to remove an item from the shopping cart.
+#When removing an item, you should verify the following:
+#Both the item name and price are removed from their respective lists.
+#The number the user enters should be converted to a 0-based index and checked to make sure it is within the bounds of the list.
+#The program allows you to remove any item from the list including the first one and the last one. (Sometimes, if you have a bug in your project you might not allow removing the last item as you should.)
+
+
+
 print("Welcome to the Shopping Cart Program!")
 print()
 print("Please select one of the following: \n 1. Add item \n 2. View cart \n 3. Remove item \n 4. Compute total \n 5. Quit")
