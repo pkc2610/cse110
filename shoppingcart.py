2397795b004d287fd94d423dacd9f77e826a3a67
pkc2610 <phoebekayc@gmail.com>
2024-02-10 19:29:13 -0700

    sometimes in order to get things done, you have to really confidently write a bunch of really bad code

--- shoppingcart.py
+++ shoppingcart.py
@@ -2,7 +2,24 @@ print("Welcome to the Shopping Cart Program!")
 print()
 print("Please select one of the following: \n 1. Add item \n 2. View cart \n 3. Remove item \n 4. Compute total \n 5. Quit")
 
+shoppinglist = []
+item = None
+
 action=input("Please select an action: ")
 
-if action == 1:
-    input("What item would you like to add?")
+while action != "5":
+    
+    if action == 1:
+     item=input("What item would you like to add?")
+     shoppinglist.append(item)
+
+    elif action == 2:
+       print(f"{shoppinglist}")
+
+    elif action ==3:
+       info=input("What item would you like to remove?")
+       shoppinglist.append(info)
+    
+    elif action == 4:
+       print("Process incoming")
+
