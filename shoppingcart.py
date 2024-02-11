5888ab69c403bdb3ec844c832389982c976693b5
pkc2610 <phoebekayc@gmail.com>
2024-02-10 19:17:42 -0700

    action time bb yeah

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,3 +1,6 @@
 print("Welcome to the Shopping Cart Program!")
 print()
 print("Please select one of the following: \n 1. Add item \n 2. View cart \n 3. Remove item \n 4. Compute total \n 5. Quit")
+
+action=input("Please select an action: ")
+
