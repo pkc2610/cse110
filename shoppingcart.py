7c95fadc2fb125ecbbc9f3a5a5b691dbca991507
pkc2610 <phoebekayc@gmail.com>
2024-02-19 19:23:54 -0700

    the improvements

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,40 +1,40 @@
 print("Welcome to the Shopping Cart Program!")
 print()
-print("Please select one of the following: \n 1. Add item \n 2. View cart \n 3. Remove item \n 4. Compute total \n 5. Quit")
 
 shoppinglist = []
 listprices = []
 item = None
-
-action=int(input("Please select an action: "))
+action = 0
     
 while action != 5:
+    print("Please select one of the following: \n 1. Add item \n 2. View cart \n 3. Remove item \n 4. Compute total \n 5. Quit")
+    action=int(input("Please select an action: "))
 
     if action == 1:
      item=input("What item would you like to add? ")
      itemprice=float(input(f"What is the price of {item}? "))
      shoppinglist.append(item)
      listprices.append(itemprice)
-
-     action=int(input("Please select an action: "))
+     print(f"{item} has been added to your cart.")
 
     elif action == 2:
        #could maybe put a for loop in here if I'm really feeling smort--minute marker 6:15 of the first video 
-         print("The shopping list and prices are: ")
+         print("The contents of the shopping cart are:")
          for i in range(len(shoppinglist)):
-            print(f"{shoppinglist[i]}: ${listprices[i]}")
-         action=int(input("Please select an action: "))
+            print(f"{i+1}: {shoppinglist[i]} - ${listprices[i]}")
 
     elif action ==3:
-       info=input("What item would you like to remove? ")
-       index = shoppinglist.index(info)
-       shoppinglist.pop(index)
-       listprices.pop(index)
-       action=int(input("Please select an action: "))
+       info=int(input("What item would you like to remove? "))
+       if info >= len(shoppinglist) or info < 0:
+          print("Sorry, that is not a valid item number.")
+       else:
+          del shoppinglist[int(info) - 1]
+          del listprices[int(info) - 1]
     
     elif action == 4:
        total = 0.0
        for i in listprices:
           total += i
-       print(f"Your total is: {total}")
-       action=int(input("Please select an action: "))
+       print(f"The total price of the items in the shopping cart is ${total:.2f}")
+    print()
+print("Thank you! Goodbye.")
